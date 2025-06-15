99e4e0dcdd2000cafa54fad6b42e80b1fff97e86
AryaRizky <aryarizkytriputra230304@gmail.com>
2025-06-15 16:38:27 +0700

    das

--- sales/show.php
+++ sales/show.php
@@ -53,10 +53,7 @@ mysqli_data_seek($sale_details, 0);
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center mb-4">
-                            <h5 class="card-title m-0">Sale: <?= htmlspecialchars($sale['code']) ?></h5>
-                            <a href="index.php" class="btn btn-secondary">
-                                <i class='bx bx-arrow-back'></i> Back to List
-                            </a>
+                            <h5 class="card-title m-0"><?= htmlspecialchars($sale['code']) ?></h5>
                         </div>
                         
                         <?php if (isset($_GET['success'])): ?>
