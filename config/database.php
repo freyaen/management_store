b9a3d8cd561fbba1ff5876137feb8a31b246c935
AryaRizky <55610152+Aryaak@users.noreply.github.com>
2025-06-15 15:36:32 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,19 +1,11 @@
 <?php
-if (file_exists(__DIR__ . '/../.env')) {
-    $lines = file(__DIR__ . '/../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue; // skip komentar
-        list($key, $value) = explode('=', $line, 2);
-        $_ENV[trim($key)] = trim($value);
-    }
-}
-
-$host = $_ENV['DB_HOST'] ?? 'localhost';
-$user = $_ENV['DB_USER'] ?? 'root';
-$pass = $_ENV['DB_PASS'] ?? '';
-$db   = $_ENV['DB_NAME'] ?? '';
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "ECSUEfPJSTyGRjeIXyRQkcSligREPJmY";
+$db   = "railway";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+  die("Connection failed: " . mysqli_connect_error());
 }
+?>
